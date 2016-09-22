10626b3c6cd158835a76fa914c5a14902ac95def
Krafus Walker <krafuswalker@gmail.com>
2016-09-21 20:19:39 -0400

    Added continue and formatted output and handeled decimal format

--- Lab1.java
+++ Lab1.java
@@ -1,37 +1,54 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
-import java.util.Scanner;   // Import Scanner utility
 public class Lab1 {
 
 	/**
-	 * @param args
+	 * @author Krafus Walker
+	 * @Program to calculate Perimeter and Area of various classrooms at Grand
+	 *          Circus based on user input.
 	 */
+	// Update to add contiue and did formatting
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(" Learn your squares and cubes!");
-		Scanner sc = new Scanner(System.in);                  //Create a scanner named sc
-		String yesOrNo;										  //Declare a string variable to handle user yes no answers
-		do													  // Start a do loop to process and print table 
-		{
-			
-			System.out.println("Enter an integer:");          // Ask user for a number to process up to starting at 1
-			int lastnum = sc.nextInt();                       // Declare and assign value to the number to process up to
-			System.out.println("Number" + "\t\t\t" + "Squared" + "\t\t\t" + "Cubed");       // Print column headings
-			System.out.println("======" +  "\t\t\t" + "=======" + "\t\t\t" + "======");     // Print column decor
-			
-			for (int i=1;i <= lastnum; i++)										//start for  funtion to increment integer by 1 
-			{
-				int sq = i * i; 												//declare and calculate Square root numbers
-				int cb = i * i * i;                                             //declare and calculate cubed numbers
-				System.out.println(i + "\t\t\t" + sq + "\t\t\t" + cb);          //  print calclated results
-			}
-			System.out.println("Do you want to continue?");						// Ask for and get while condition
-			 yesOrNo = sc.next();
-			
-			
-		} while (yesOrNo.equals("y"));                                          // set and check for while condition
-	
-	System.out.println("Good-bye. Good-Thank you for using our code");		
-	
+		// Prompt user to enter values
+
+		Scanner scanner = new Scanner(System.in);
+		String choice = "y";
+		DecimalFormat decFor = new DecimalFormat("0.##");
+
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.println(" ");
+			System.out.println("Please Enter The Classroom Length:");
+
+			double length = scanner.nextDouble();
+
+			System.out.println("Please Enter The Classroom Width:");
+			double width = scanner.nextDouble();
+			double area = width * length;
+			double perimeter = 2 * width + 2 * length;
+			// print out results for area and perimeter
+			System.out.print("The Classroom Area Is: ");
+			System.out.println(decFor.format(area));
+			System.out.print("The Classroom Perimeter Is: ");
+			System.out.println(decFor.format(perimeter));
+			System.out.println();
+			System.out.println("Now Let's Calculate The Volume.");
+			System.out.println("Please Enter The Classroom Height:");
+			double height = scanner.nextDouble();
+
+			System.out.print("The Volume Of Classroom Is:  ");
+			double Volume = height * width * length;
+			System.out.println(decFor.format(Volume));
+			System.out.println(" ");
+
+			// see if customer wants to continue
+			System.out.print("Continue? (y/n): ");
+			System.out.print(" ");
+			choice = scanner.next();
+		}
+		scanner.close();
+		System.out.println("Good-bye");
 	}
 
 }
